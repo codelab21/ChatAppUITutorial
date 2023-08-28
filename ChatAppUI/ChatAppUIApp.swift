824c99ec6a91218cb7bd5b808d5d80aeac61cdf6
codelab21 <codelab21@outlook.com>
2023-08-28 11:58:58 +0300

    Initial Commit

---
+++ ChatAppUI/ChatAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppUIApp.swift
+//  ChatAppUI
+//
+//  Created by Eymen on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
